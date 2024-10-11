3cf3e0de30cbe7b60f0d718c2b60e0c756ba1524
Vyacheslav <slavaotorvin@gmail.com>
2024-10-11 09:28:09 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+
+namespace Parsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"Аргументы = {arg}");
+            }
+
+        }
+
+    }
+}
+
